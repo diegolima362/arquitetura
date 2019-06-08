d13a75bbb478aa8baec5d5a15ba55bd20dfdc728
Bell Coutinho <thebell@localhost.localdomain>
2019-06-08 19:09:12 -0300

    Atualizando

--- Cliente.java
+++ Cliente.java
@@ -1,17 +1,27 @@
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.NoSuchElementException;
 
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
+	private static ObjectOutputStream output;
+	private static ObjectInputStream input;
 	private int codigo;
     private String nome;
-    private Endereco endereco;
+    //private Endereco endereco;
     private int telefone;
     
     public Cliente() {}
-    public Cliente(String nome, int codigo, Endereco endereco, int telefone) {
+    public Cliente(String nome, int codigo, /*Endereco endereco*/ int telefone) {
         this.nome = nome;
         this.codigo = codigo;
-        this.endereco = endereco;
+        //this.endereco = endereco;
         this.telefone = telefone;
     }
 
@@ -27,12 +37,12 @@ public class Cliente implements Serializable {
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public Endereco getEndereco() {
-        return endereco;
-    }
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
+//    public Endereco getEndereco() {
+//        return endereco;
+//    }
+//    public void setEndereco(Endereco endereco) {
+//        this.endereco = endereco;
+//    }
     public int getTelefone() {
         return telefone;
     }
@@ -41,4 +51,81 @@ public class Cliente implements Serializable {
             this.telefone = telefone;
         }
     }
+    
+	public static void escrever(Cliente cliente) {
+    	abrirArquivoE();
+    	adicionarE(cliente);
+    	fecharArquivoE();
+    	
+    }
+    private static void abrirArquivoE() {
+		try {
+			output = new ObjectOutputStream(Files.newOutputStream(Paths.get("./bin/clientes.obj"), StandardOpenOption.CREATE,StandardOpenOption.APPEND));
+		}
+		catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+    private static void adicionarE(Cliente cliente) {
+		try {
+			output.reset();
+			output.writeObject(cliente);
+		}
+		catch (NoSuchElementException elementException) {
+			elementException.printStackTrace();
+		}
+		catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+	private static void fecharArquivoE() {
+		try {
+			if (output != null)
+				output.close();
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+	public static void ler() {
+		abrirArquivoL();
+		lerArquivoL();
+		fecharArquivoL();
+	}
+    private static void abrirArquivoL() {
+		try {
+			input = new ObjectInputStream(Files.newInputStream(Paths.get("./bin/clientes.obj")));
+		}
+		catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+    private static void lerArquivoL() {
+    	while (true) {
+	    	try {
+			
+				Cliente cliente = (Cliente) input.readObject();
+				System.out.printf("Nome: %s\n", cliente.getNome());
+				System.out.printf("Código: %d\n", cliente.getCodigo());
+				System.out.printf("Telefone: %d\n", cliente.getTelefone());
+	    	}
+	    	catch (EOFException endOfFileException) {
+	    		endOfFileException.printStackTrace();
+	    	}
+	    	catch (ClassNotFoundException classNotFoundException) {
+	    		classNotFoundException.printStackTrace();
+	    	}
+	    	catch (IOException ioException) {
+	    		ioException.printStackTrace();
+	    		break;
+	    	}
+    	}
+	}
+	private static void fecharArquivoL() {
+		try {
+			if (input != null)
+				input.close();
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
 }
